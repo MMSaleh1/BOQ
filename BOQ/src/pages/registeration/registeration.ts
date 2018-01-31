7b96c9098a0f9eb150fe412a1d0042d6e994017e
DESKTOP-SVEKDHH\edge <mohammed.magdy.ali.96@gmail.com>
2018-01-31 14:19:22 +0200

    updates to the view pages : login registration landing side menu

--- BOQ/src/pages/registeration/registeration.ts
+++ BOQ/src/pages/registeration/registeration.ts
@@ -31,7 +31,7 @@ export class RegisterationPage {
     this.registerForm = this.formBuilder.group({
       name : ['',[Validators.required,Validators.maxLength(12),Validators.minLength(4)]],
       password : ['',[Validators.required,Validators.maxLength(20),Validators.minLength(6)]],
-      email : ['',[Validators.required]],
+      email : ['',[Validators.required,Validators.pattern(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)]],
       gender : ['',[Validators.required]],
       location : ['',[Validators.required]],
       phone : ['', [Validators.required, Validators.maxLength(11),Validators.minLength(11)]]
@@ -49,13 +49,17 @@ export class RegisterationPage {
         console.log(data);
         if(data.length > 0){
           this.user = new User(data[0].USERID,this.registerForm.value.name,this.registerForm.value.gender,this.registerForm.value.location,this.registerForm.value.password,this.registerForm.value.email,this.registerForm.value.phone)
-        }
+        
         console.log(this.user);
         this.natStorage.setItem('user' ,this.user);
         this.navCtrl.setRoot(HomePage , {"user" : this.user});
+        }else{
+          alert("Server Error");
+        }
       },err=>{
-        console.log(err);
+        alert("No Connection");
       })
+    }else{
     }
   }
 
